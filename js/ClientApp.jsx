9eb7ed58d1c245148c29171d35b87d7cc016e624
Cristian Echeverria <cristian.echeverri4@gmail.com>
2018-05-10 09:04:32 -0600

    Intro to State Management

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,18 +1,21 @@
-const ce = React.createElement;
+import React from 'react';
+import {render} from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Landing from './Landing';
+import Search from './Search';
+import FourOhFour from './FourOhFour';
 
-const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-};
+const App = () => (
+  <BrowserRouter>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Landing}/>
+        <Route path="/search" component={Search}/>
+        <Route component={FourOhFour}/>
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
 
-const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'peru' })
-  );
-};
-
-ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));
+render(
+  <App/>, document.getElementById('app'));
